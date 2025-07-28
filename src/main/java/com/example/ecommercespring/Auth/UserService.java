892f86868dbf781f55fd4d967ae515d34fda6078
Abdelhamid Salah <abdelhamidsalah222@gmail.com>
2025-07-28 15:36:11 +0300

    Add Category and idss and put

--- src/main/java/com/example/ecommercespring/Auth/UserService.java
+++ src/main/java/com/example/ecommercespring/Auth/UserService.java
@@ -1,11 +1,7 @@
 package com.example.ecommercespring.Auth;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Collections;
 
 @Service
